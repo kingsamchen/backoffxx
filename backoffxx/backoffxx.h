cb5d518fbbafd6822b5b53af0073c77d1b3b4cfb
Kingsley Chen <kingsamchen@gmail.com>
2021-08-05 00:16:08 +0800

    Init project

---
+++ backoffxx/backoffxx.h
@@ -0,0 +1,9 @@
+
+#ifndef BACKOFFXX_BACKOFFXX_H_
+#define BACKOFFXX_BACKOFFXX_H_
+
+inline bool test() {
+    return true;
+}
+
+#endif // BACKOFFXX_BACKOFFXX_H_
---
+++ tests/demo_test.cpp
@@ -0,0 +1,8 @@
+
+#include "gtest/gtest.h"
+
+#include "backoffxx/backoffxx.h"
+
+TEST(HelloTest, BasicAssertions) {
+  EXPECT_TRUE(test());
+}
